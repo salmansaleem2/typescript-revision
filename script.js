8419f2b14c9c8623751acd5f58ab95e3f5c94eab
salmansaleem2 <salmansaleem8523@gmail.com>
2024-05-19 14:17:41 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,26 @@
+"use strict";
+// let greetings: string = "Helo Salman";
+Object.defineProperty(exports, "__esModule", { value: true });
+// // number
+// let userId: any = 334455.3;
+// // boolean
+// let isLoggedIn: boolean = false;
+// // any
+// let hero;
+// console.log(userId);
+function addTwo(num) {
+    return num + 2;
+}
+function getUpper(val) {
+    return val.toUpperCase();
+}
+function signUpUser(name, email, isPaid) {
+    return;
+}
+var userSignIn = function (name, email, isPaid) {
+    if (isPaid === void 0) { isPaid = false; }
+};
+userSignIn("salman", "salman@gmail.com");
+signUpUser("salman", "salman@gmail", true);
+getUpper("salman");
+addTwo(5);
---
+++ script.ts
@@ -0,0 +1,37 @@
+// let greetings: string = "Helo Salman";
+
+// // number
+
+// let userId: any = 334455.3;
+
+// // boolean
+
+// let isLoggedIn: boolean = false;
+
+// // any
+// let hero;
+
+// console.log(userId);
+
+function addTwo(num: number): number {
+  return num + 2;
+  // return true;
+}
+
+function getUpper(val: string) {
+  return val.toUpperCase();
+}
+
+function signUpUser(name: string, email: string, isPaid: boolean) {
+  return;
+}
+const userSignIn = (name: string, email: string, isPaid: boolean = false) => {};
+
+userSignIn("salman", "salman@gmail.com");
+
+signUpUser("salman", "salman@gmail", true);
+
+getUpper("salman");
+addTwo(5);
+
+export {};
