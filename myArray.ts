6ae5192846ef89544a5f9a16ffeafaa5ae4bea81
salmansaleem2 <salmansaleem8523@gmail.com>
2024-05-19 16:29:22 +0500

    prac: Array

---
+++ myArray.ts
@@ -0,0 +1,17 @@
+const superHeros: string[] = [];
+
+const heroPower: Array<number> = [];
+
+superHeros.push("spiderman");
+heroPower.push(23);
+
+type User = {
+  name: string;
+  isActive: boolean;
+};
+
+const allUsers: User[] = [];
+
+const MLModels: number[][] = [[255, 255, 255], []];
+
+allUsers.push({ name: "", isActive: false });
