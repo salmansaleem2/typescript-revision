275a91c4ed6a0834998acdfef41a12b788da22c1
salmansaleem2 <salmansaleem8523@gmail.com>
2024-05-19 14:47:54 +0500

    bad behavior of objects

---
+++ myObject.js
@@ -0,0 +1,13 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var User = {
+    name: "salman",
+    email: "salmansaleem8523@gmail.com",
+    isActive: true,
+};
+function createUser(_a) {
+    var string = _a.name, boolean = _a.isPaid;
+    // console.log(name, isPaid);
+    // return { name, isPaid };
+}
+createUser({ name: "salman", isPaid: true });
---
+++ myObject.ts
@@ -0,0 +1,16 @@
+const User = {
+  name: "salman",
+  email: "salmansaleem8523@gmail.com",
+  isActive: true,
+};
+
+function createUser({ name: string, isPaid: boolean }) {}
+
+let newUser = { name: "salman", isPaid: true ,email,"salman@gamil.com"};
+
+createUser({ name: "salman", isPaid: true });
+
+function createCourse(): { name: string; price: number } {
+  return { name: "reactjs", price: 399 };
+}
+export {};
