4046c5d03588c867f568db799db6f92082777d38
salmansaleem2 <salmansaleem8523@gmail.com>
2024-05-19 16:11:38 +0500

    prac: readonly and optional

--- myObject.js
+++ myObject.js
@@ -1,13 +1,15 @@
 "use strict";
+// const User = {
+//   name: "salman",
+//   email: "salmansaleem8523@gmail.com",
+//   isActive: true,
+// };
 Object.defineProperty(exports, "__esModule", { value: true });
-var User = {
+var myUser = {
+    _id: "1234",
     name: "salman",
-    email: "salmansaleem8523@gmail.com",
+    email: "salman@gmail.com",
     isActive: true,
 };
-function createUser(_a) {
-    var string = _a.name, boolean = _a.isPaid;
-    // console.log(name, isPaid);
-    // return { name, isPaid };
-}
-createUser({ name: "salman", isPaid: true });
+myUser.email = "s@gmail.com";
+myUser._id = "asa";
--- myObject.ts
+++ myObject.ts
@@ -1,22 +1,51 @@
-const User = {
+// const User = {
+//   name: "salman",
+//   email: "salmansaleem8523@gmail.com",
+//   isActive: true,
+// };
+
+// function createUser({ name: string, isPaid: boolean }) {}
+
+// let newUser = { name: "salman", isPaid: true, email: "salman@gamil.com" };
+
+// createUser(newUser);
+
+// type NewUser = {
+//   name: string;
+//   price: number;
+// };
+
+// function createCourse(user: NewUser): { name: string; price: number } {
+//   return { name: "reactjs", price: 399 };
+// }
+// createCourse({ name: "", price: 499 });
+
+type User = {
+  readonly _id: string;
+  name: string;
+  email: string;
+  isActive: boolean;
+  credcardDetails?: number;
+};
+
+let myUser: User = {
+  _id: "1234",
   name: "salman",
-  email: "salmansaleem8523@gmail.com",
+  email: "salman@gmail.com",
   isActive: true,
 };
 
-function createUser({ name: string, isPaid: boolean }) {}
+myUser.email = "s@gmail.com";
+// myUser._id = "asa";
 
-let newUser = { name: "salman", isPaid: true, email: "salman@gamil.com" };
-
-createUser(newUser);
-
-type NewUser = {
-  name: string;
-  price: number;
+type cardNumber = {
+  cardnumber: string;
 };
 
-function createCourse(user: NewUser): { name: string; price: number } {
-  return { name: "reactjs", price: 399 };
-}
-createCourse({ name: "", price: 499 });
+type cardDate = {
+  carddate: string;
+};
+
+type cardDetails = cardNumber & cardDate & { cvv: string };
+
 export {};
