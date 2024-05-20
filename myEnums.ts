1739bae948e348ad9cbf66f8c75506d43b5c1e81
salmansaleem2 <salmansaleem8523@gmail.com>
2024-05-21 02:39:27 +0500

    prac: enums

---
+++ myEnums.ts
@@ -0,0 +1,8 @@
+enum SeatChoice {
+  AISLE = "aisle",
+  MIDDLE = 3,
+  Window,
+  FOURTH,
+}
+
+const hc = SeatChoice.AISLE;
--- myTupEnums.ts
+++ myTupEnums.ts
@@ -9,4 +9,4 @@ type User1 = [number, string];
 const newUser: User1 = [112, "test"];
 
 newUser[1] = "hc.com";
-newUser.push(true);
+// newUser.push(true);
